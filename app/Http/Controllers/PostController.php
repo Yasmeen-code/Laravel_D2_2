9b54ce6e57730801e369abd55e78c127e2b1e84a
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-16 16:41:49 +0300

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
